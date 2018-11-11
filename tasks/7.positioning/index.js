8ac268168a79dfd992e347ef26293733c862ab96
aminopyridin <aminopyrodin@gmail.com>
2018-11-11 19:42:21 +0500

    add positioning task

---
+++ tasks/7.positioning/index.js
@@ -0,0 +1,7 @@
+/*
+    Изменить элементу цвет и ширину можно вот так:
+
+    const element = document.querySelector('.myElement');
+    element.style.color = 'red';
+    element.style.width = '300px';
+*/
