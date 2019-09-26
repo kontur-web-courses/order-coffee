7fc17f6b9bf68d0a92677f0b11acf96bdb02cdd2
nulladdict <nulladdict@ya.ru>
2019-09-26 14:50:51 +0500

    feat: update tasks & lessons

---
+++ tasks/4.world-wide-west/SpeedRate.js
@@ -0,0 +1,16 @@
+const SpeedRate = function() {
+    let speedRate = 1;
+
+    function set(value) {
+        speedRate = value;
+    }
+
+    function get() {
+        return speedRate;
+    }
+
+    return {
+        set,
+        get
+    };
+}();
