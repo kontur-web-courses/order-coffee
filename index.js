816978232531a67dea30f5d560c911839590d9a2
Nikita <nikitavedeshkin3@gmail.com>
2020-11-10 20:38:58 +0500

    refactoring 1-6

--- index.js
+++ index.js
@@ -1,68 +1,95 @@
-let addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', addBeverage);
+const beverages = document.querySelector('.beverages');
+const beverageTemplate = document.getElementById('beverage-template').cloneNode(true);
+beverageTemplate.removeAttribute('id');
+document.getElementById('beverage-template').remove();
 
-let countBeverage = 1;
+const modal = document.getElementById('modal');
+const resultParagraph = modal.querySelector('p');
+const resultTable = modal.querySelector('table')
+
+let countBeverage = 0;
+let globalId = 0;
 
 function addBeverage() {
-    const beverages = document.querySelectorAll('.beverage');
-    const beverage = beverages[beverages.length - 1];
-
-    const newBeverage = beverage.cloneNode(true);
+    const newBeverage = beverageTemplate.cloneNode(true);
     countBeverage++;
-
-    const h4 = newBeverage.querySelector('.beverage-count');
-    h4.textContent = h4.textContent.substr(0, 9) + countBeverage;
-
-    const inputs = newBeverage.querySelectorAll('input');
-
+    const header = newBeverage.querySelector('.beverage-count');
+    header.textContent += countBeverage;
+    const inputs = newBeverage.querySelectorAll('input[name]');
     for (let input of inputs) {
-        input.name = input.name + countBeverage;
+        input.name = `${input.name}_${globalId}`;
     }
-
-    beverage.after(newBeverage);
+    globalId++;
+    beverages.append(newBeverage);
 }
 
-function closeBeverage(fieldset) {
+function closeBeverage(beverage) {
     if (countBeverage > 1) {
-        fieldset.remove();
+        beverage.remove();
         countBeverage--;
-        const h4s = document.querySelectorAll('h4');
-        for (let i = 0; i < h4s.length; i++) {
-            h4s[i].textContent = h4s[i].textContent.substr(0, 9) + (i + 1);
+        let templateString = beverageTemplate.querySelector('.beverage-count').textContent;
+        const headers = document.querySelectorAll('.beverage-count');
+        for (let i = 0; i < headers.length; i++) {
+            headers[i].textContent = templateString + (i + 1);
         }
     }
 }
 
-var openDialogButton = document.getElementById('successButton');
-var cancelCross = document.getElementById('cancelModalCross');
-var favDialog = document.getElementById('favDialog');
-var resultsParagraph = document.getElementById('results');
+function openModal() {
+    updateParagraph();
+    updateTable();
+    modal.showModal();
+}
 
-openDialogButton.addEventListener('click', function () {
-    favDialog.showModal();
-    let bool1 = Math.round(countBeverage / 10) % 10 == 1 || countBeverage % 10 >= 5 || countBeverage % 10 == 0;
-    let bool2 = countBeverage % 10 == 1;
-    let nap = 'напит' + (bool1 ? 'ков' : (bool2 ? 'ок' : 'ка'));
-    resultsParagraph.textContent = `Вы заказали ${countBeverage} ` + nap;
+function closeModal() {
+    modal.close();
+}
 
-    let tableHTML = "<tr> <th>Напиток</th> <th>Молоко</th> <th>Дополнительно</th> </tr>";
+function updateParagraph() {
+    let endingFlag1 = Math.round(countBeverage % 100 / 10) == 1
+        || countBeverage % 10 >= 5
+        || countBeverage % 10 == 0;
+    let endingFlag2 = countBeverage % 10 == 1;
+    let word = 'напит' + (endingFlag1 ? 'ков' : (endingFlag2 ? 'ок' : 'ка'));
+    resultParagraph.textContent = `Вы заказали ${countBeverage} ${word}`;
+}
 
-    let beverages = document.querySelectorAll('.beverage');
-    
-    for (let beverage of beverages) {
-        let inputs = beverage.querySelectorAll('input:checked');
-        let select = beverage.querySelector('select');
-        tableHTML += `<tr><td>${select.value}</td><td>${inputs[0].value}</td><td>`;
-        for (let i = 1; i < inputs.length; i++) {
-            tableHTML += `${inputs[i].value}`;
-            if (i + 1 < inputs.length)
-                tableHTML += ', ';
-        }
-        tableHTML += '</td></tr>';   
+function updateTable() {
+    const tbody = resultTable.querySelector('tbody');
+    while (tbody.children.length != 1) {
+        tbody.removeChild(tbody.lastChild);
     }
-    document.getElementById('resultsTable').innerHTML = tableHTML;
-});
+    for (let beverage of beverages.children) {
+        let select = beverage.querySelector('select');
+        let radio = beverage.querySelector('input:checked[type=radio]');
+        let checkBoxes = beverage.querySelectorAll('input:checked[type=checkbox]');
+        let text = '';
+        for (let i = 0; i < checkBoxes.length; i++) {
+            text += `${checkBoxes[i].value}`;
+            if (i + 1 < checkBoxes.length)
+                text += ', ';
+        }
+        const tr = document.createElement('tr');
+        tr.append(createTd(select.value));
+        tr.append(createTd(radio.value));
+        tr.append(createTd(text));
+        tbody.append(tr);
+    }
+}
 
-cancelCross.addEventListener('click', function () {
-    favDialog.close();
-});
+function createTd(text) {
+    const td = document.createElement('td');
+    td.textContent = text;
+    return td;
+}
+
+document.getElementById('successButton')
+    .addEventListener('click', openModal);
+
+modal.querySelector('.cross')
+    .addEventListener('click', closeModal);
+
+document.querySelector('.add-button')
+    .addEventListener('click', addBeverage);
+
+addBeverage();
