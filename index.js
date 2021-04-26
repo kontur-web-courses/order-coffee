a424d20895be6baedff4194f0f9590ebb79a15f9
lev <levtrifonov@mail.ru>
2021-04-26 14:49:56 +0500

    new

---
+++ index.js
@@ -0,0 +1,10 @@
+const beverage = document.querySelector('beverage');
+let beverageCount = 1;
+let addButton = document.querySelector('add-button');
+
+function addButtonHendler(){
+    let newBeverage = object.copy(beverage);
+    beverageCount++;
+    newBeverage.querySelector('beverage-count').textContent
+}
+
