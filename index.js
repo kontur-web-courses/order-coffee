df6c46db427a78c0f506a470284d3ae3ac05168f
lev <levtrifonov@mail.ru>
2021-04-26 15:50:09 +0500

    123

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ let beverage = document.querySelector('.beverage');
 let beverageCount = 1;
 const addButton = document.querySelector('.add-button');
 
-function addButtonHandler(){
+function closeButtonHandler(currentBeverage) {
+    currentBeverage.remove();
+}
+
+function addButtonHandler() {
     let newBeverage = beverage.cloneNode(true);
     beverageCount++;
     newBeverage.querySelector('.beverage-count').textContent = `Напиток №${beverageCount}`;
-    if(beverageCount == 2){
-        newBeverage.insertAdjacentHTML('afterBegin', )
+    if (beverageCount == 2) {
+        newBeverage.insertAdjacentHTML('afterBegin', '<button style="margin-left: 95%" id="button-close">X</button>');
     }
     addButton.insertAdjacentElement('beforeBegin', newBeverage);
+    newBeverage.querySelector('#button-close')
+        .addEventListener('click', (event) => closeButtonHandler(newBeverage));
     beverage = newBeverage;
 }
 
