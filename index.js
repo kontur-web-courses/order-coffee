87caa21281f6cafd60bb2ca74895c454b9b37fd4
SergeyLiyasov <liseand2002@gmail.com>
2022-04-18 16:26:16 +0500

    Модальное окно

--- index.js
+++ index.js
@@ -20,3 +20,17 @@ for (let b of removeBeverageButton) {
         }    
     })
 }
+
+function overlay_on() {
+    const overlay = document.querySelector('.overlay');
+    const lightbox = document.querySelector('.lightbox');
+    overlay.style.display = 'flex';
+    lightbox.style.display = 'block';
+}
+
+function overlay_off() {
+    const overlay = document.querySelector('.overlay');
+    const lightbox = document.querySelector('.lightbox');
+    overlay.style.display = 'none';
+    lightbox.style.display = 'none';
+}
