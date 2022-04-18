b135069e85b88ad0d85923475b96d6e9a02c06fe
SergeyLiyasov <liseand2002@gmail.com>
2022-04-18 16:19:26 +0500

    Добавление нового анпитка и "готово" остаются внизу

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-let form = document.querySelector('form');
+let lastSubForm = document.querySelector('form .beverage');
+let beverageCount = document.querySelectorAll('.beverage').length;
 
 document.querySelector('.add-button')
     .addEventListener('click',
     function (event) {
-        let newSubForm = form.querySelector('.beverage').cloneNode(true);
+        let newSubForm = lastSubForm.cloneNode(true);
         let header = newSubForm.querySelector('.beverage-count');
         header.textContent = `Напиток №${++beverageCount}`;
-        form.appendChild(newSubForm);
+        lastSubForm.after(newSubForm);
+        lastSubForm = newSubForm;
     });
 
 
 let removeBeverageButton = document.getElementsByClassName('remove-beverage');
-let beverageCount = document.querySelectorAll('.beverage').length;
 for (let b of removeBeverageButton) {
     b.addEventListener("click", function(event) {
         if (beverageCount > 1) {
