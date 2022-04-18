366d27c3123af721306c35eb79ad9374abc0efe6
SergeyLiyasov <liseand2002@gmail.com>
2022-04-18 16:07:43 +0500

    корректная нумерация напитков

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let form = document.querySelector('form');
+let beverageCount = 1;
 
 document.querySelector('.add-button')
     .addEventListener('click',
     function (event) {
         let newForm = form.cloneNode(true);
+        let header = newForm.querySelector('.beverage-count');
+        header.textContent = `Напиток №${++beverageCount}`;
         document.body.appendChild(newForm);
     });
