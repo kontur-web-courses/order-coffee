748250ca69b33d9efd72f9b9b6ba866e7f7bf684
YOUR_NAME <your@email.com>
2022-04-18 18:23:08 +0500

    1-6 done

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 drinkCounter = 1;
 
 document.querySelector(".add-button")
-        .addEventListener("click", () => {
-    let forms = document.querySelectorAll(".beverage");
-    let newForm = forms[forms.length - 1].cloneNode(true);
-    let headerText = newForm.querySelector("h4").innerHTML;
-    drinkCounter += 1;
-    newForm.querySelector("h4").innerHTML = `Напиток №${drinkCounter}`;
-    for (let radio of newForm.querySelectorAll("input[type=radio]")) {
-        radio.name = "milk" + drinkCounter;
-    }
-    forms[forms.length - 1].after(newForm);
-});
+    .addEventListener("click", () => {
+        let forms = document.querySelectorAll(".beverage");
+        let newForm = forms[forms.length - 1].cloneNode(true);
+        drinkCounter += 1;
+        newForm.querySelector("h4").innerHTML = `Напиток №${drinkCounter}`;
+        for (let radio of newForm.querySelectorAll("input[type=radio]")) {
+            radio.name = "milk" + drinkCounter;
+        }
+        forms[forms.length - 1].after(newForm);
+    });
 
 function removeDrink(target) {
     if (document.querySelectorAll(".beverage").length > 1) {
@@ -25,26 +24,70 @@ function removeDrink(target) {
 }
 
 document.querySelector(".submit-button")
-        .addEventListener("click", (e) => {
-    document.querySelector("#drinkConfDialog").showModal();
+    .addEventListener("click", (e) => {
+        document.querySelector("#drinkConfDialog").showModal();
 
-    document.querySelector(".modal-content").innerHTML = `Вы заказали ${drinkCounter} ${endOfBeverage()}`;
-    document.querySelector(".close-dialog")
+        document.querySelector(".modal-content").innerHTML = `Вы заказали ${drinkCounter} ${endOfBeverage()}`;
+        constructTable();
+        document.querySelector(".close-dialog")
             .addEventListener("click", (e) => {
-        document.querySelector("#drinkConfDialog").close();
+                document.querySelector("#drinkConfDialog").close();
+            });
     });
-});
 
 function endOfBeverage() {
     dC = drinkCounter % 100;
     if (dC > 10 && dC < 20) {
-      return "напитков";
+        return "напитков";
     }
     if (dC % 10 > 1 && dC % 10 < 5) {
-      return "напитка";
+        return "напитка";
     }
     if (dC % 10 == 1) {
-      return "напиток";
+        return "напиток";
     }
     return "напитков";
+}
+
+
+function constructTable() {
+  let table = document.querySelector("table");
+  table.innerHTML = "";
+  table.border = 12;
+  table.style.borderColor = "aquamarine";
+
+
+  let header = document.createElement("tr");
+  header.classList.add("order-cell");
+  for (let headerStr of ["Напиток", "Молоко", "Дополнительно"]) {
+    let column = document.createElement("th");
+    column.innerHTML = headerStr;
+    header.append(column);
   }
+  table.append(header);
+
+  for (let form of document.querySelectorAll(".beverage")) {
+    let line = document.createElement("tr");
+    let drink = document.createElement("td");
+
+    let drinkName = '';
+    let select = form.getElementsByTagName('select')[0];
+    for (let i = 0; i < 4; i++)
+        if (select[i].selected === true)
+            drinkName = select[i].innerHTML;
+
+    drink.innerHTML = drinkName;
+    line.append(drink);
+
+    let milk = document.createElement("td");
+    milk.innerHTML = form.querySelector("input[type=radio]:checked + span").innerHTML;
+    line.append(milk);
+
+    let additional = document.createElement("td");
+    for (let additionalObj of form.querySelectorAll("input[type=checkbox]:checked + span")) {
+        additional.innerHTML += `${additionalObj.innerHTML}\n`;
+    }
+    line.append(additional);
+    table.append(line);
+  }
+}
