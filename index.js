33ef76438234176f99eff78cd39a91f574cd4eb3
lessy2014 <lessik03@gmail.com>
2022-04-18 11:22:33 +0500

    some part of third task

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ document.querySelector(".add-button").addEventListener('click', () => {
     forms[forms.length - 1].after(newForm);
 });
 
-// document.querySelector('.submit-button').addEventListener('click', () => {
-//
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.querySelector(".submit-button");
+btn.onclick = e => e.preventDefault();
+// btn.parentElement.submit(function(e) {
+//     e.preventDefault();
 // })
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
