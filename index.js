ccd3fa877828ecd7af1c267a57ea22ee21c82a67
Nastja0 <anastasizta_mgn3003@inbox.ru>
2022-04-21 18:43:43 +0500

    Мельниковы, Бикбулатов
    Мельниковы, Бикбулатов

--- index.js
+++ index.js
@@ -3,24 +3,27 @@ let number = 1;
 document.querySelector('.add-button')
     .addEventListener('click', function (event) {
     let deepCopy = form.cloneNode(true);
-    console.log(deepCopy)
     number += +1;
     deepCopy.querySelector('h4').textContent = `Напиток №${number}`;
     for (let milk of deepCopy.querySelectorAll('[name="milk"]')) {
         milk.name = `milk-${number}`;
     }
+
+    deepCopy.querySelector('.del-button')
+        .addEventListener('click', function (){
+            del_coffe(deepCopy.querySelector('.del-button'));
+        })
+
     document.querySelector('form').insertBefore(deepCopy, document.querySelector('.add-button-container'));
     event.preventDefault();
 
     })
 
-for (let del of document.querySelectorAll('.del-button')) {
-    del.addEventListener('click', function (event) {
-        if (document.querySelectorAll('.del-button').length !== 1) {
-            del.closest('fieldset').remove();
-        }
-        event.preventDefault();
-    })
+
+function del_coffe (del){
+    if (document.querySelectorAll('.del-button').length !== 1) {
+        del.closest('fieldset').remove();
+    }
 }
 
 const modalCloseBtn = document.getElementById('closeButton');
@@ -46,9 +49,27 @@ document.querySelector('.submit-button')
         let table = document.querySelector('.table');
 
         for (let order of document.querySelectorAll('.beverage')) {
-            table.innerHTML += `<tr><th>${order.querySelector('select').value}</th></tr>`
-        }
+            var sets = order.querySelectorAll('.field')
+            let text = table.insertRow()
 
+            let cell1 = text.insertCell(0)
+
+            let chek = sets[0].querySelector('select');
+            cell1.innerHTML = chek.querySelector(`option[value="${chek.value}"]`).textContent;
+
+            let cell2 = text.insertCell(1)
+            for (let milk of sets[1].querySelectorAll('label')) {
+                if (milk.querySelector('input').checked){
+                    cell2.innerHTML = milk.querySelector('span').textContent;
+                }
+            }
+            let cell3 = text.insertCell(2)
+            for (let ingredient of sets[2].querySelectorAll('label')) {
+                if (ingredient.querySelector('input').checked){
+                    cell3.innerHTML += ingredient.querySelector('span').textContent + ' ';
+                }
+            }
+        }
         modalWindow.style.display = 'block';
         event.preventDefault();
     })
