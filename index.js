780d98bd1ea6d8e4ff6beb54bb76cfb8ba146134
Evgeny Nikolaev <eannikolaev@gmail.com>
2023-04-17 15:55:19 +0500

    2 задание

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+function handleClick() {
+    if (document.querySelectorAll(".beverage").length !== 1) {
+        this.parentNode.remove();
+    }
+}
+
 const addPoint = document.querySelector(".add-button");
-const formFieldset = document.querySelector(".beverage").cloneNode(true);
 document.querySelector(".add-button").addEventListener("click", () => {
     const formFieldset = document.querySelector(".beverage").cloneNode(true);
     const len = document.querySelectorAll(".beverage").length;
     formFieldset.querySelector(".beverage-count").innerText = (`Напиток №${len + 1}`);
     addPoint.parentNode.before(formFieldset);
+    const cross = document.querySelectorAll(".cross");
+    for (const el of cross) {
+        el.addEventListener("click", handleClick);
+    }
 });
