e251c3cad9789670cf42ba83666d21244c37e44e
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2023-04-17 17:38:41 +0500

    fix second task

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 
 let formsCount = 1;
+let currFormNum = 1
 
-function addToCrossCloseEvent(){
-    let crosses = document.getElementsByClassName('form-close');
-    for (let i = 0; i < crosses.length; i++) {
-        crosses[i].addEventListener('click', (event) => {
-            if (formsCount === 1)
-                return;
-            let cureNode = crosses[i];
-            while (cureNode.tagName !== 'FORM') {
-                cureNode = cureNode.parentNode;
-            }
-            cureNode.remove();
-            formsCount--;
-        })
-    }
+function addToCrossCloseEvent(cross){
+    cross.addEventListener('click', (event) => {
+        if (formsCount === 1)
+            return;
+        let cureNode = cross;
+        while (cureNode.tagName !== 'FORM') {
+            cureNode = cureNode.parentNode;
+        }
+        cureNode.remove();
+        formsCount--;
+    });
 }
 
 
@@ -23,13 +21,14 @@ function AddButtonListener() {
     let form = document.getElementsByTagName('form')[0].cloneNode(true);
     let header = form.getElementsByClassName('beverage-count')[0];
     let button = form.getElementsByClassName('add-button')[0];
+    let cross = form.getElementsByClassName('form-close')[0];
+    addToCrossCloseEvent(cross);
     button.addEventListener('click', AddButtonListener);
-    header.textContent = `Напиток №${formsCount}`;
+    header.textContent = `Напиток №${++currFormNum}`;
     document.body.appendChild(form);
-    addToCrossCloseEvent();
 }
 
 
-addToCrossCloseEvent();
+addToCrossCloseEvent(document.getElementsByClassName('form-close')[0]);
 let addButton = document.getElementsByClassName('add-button')[0];
 addButton.addEventListener('click', AddButtonListener);
