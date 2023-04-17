d65004b4a69c0f75b7448c50ccac4740692771c8
polinabubb <polinabubb@gmail.com>
2023-04-17 18:26:47 +0500

    1,3,4,5

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ addButton.addEventListener('click', function() {
 
 document.querySelector('.submit-button').addEventListener('click',function(e){
     let modal = document.getElementById('modal');
+    let textNode = document.createTextNode(`Вы заказали ${counter} напитков`);
+    if (counter % 10 === 1 && counter != 11){
+        textNode = document.createTextNode(`Вы заказали ${counter} напиток`);
+    }
+    else if ((counter % 10 === 2 || counter % 10 === 3 || counter % 10 === 4) && counter != 12 && counter != 13 && counter != 14 ){
+        textNode = document.createTextNode(`Вы заказали ${counter} напитка`);
+    }
+    else{
+        textNode = document.createTextNode(`Вы заказали ${counter} напитков`);
+    }
+    
+    document.querySelector('.modal__content').appendChild(textNode);
     e.preventDefault();
     modal.classList.add('modal_active');
 });
@@ -20,6 +32,7 @@ document.querySelector('.submit-button').addEventListener('click',function(e){
 document.querySelector('.modal__close-button').addEventListener('click', function(e){
     let modal = document.getElementById('modal');
     modal.classList.remove('modal_active');
-    document.querySelector('.submit-button').send
+    location.reload();
+    //document.querySelector('.modal__content').removeChild();
 })
 
