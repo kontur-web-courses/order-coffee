8156852291d3d3b197992852463aac85dea9b26b
Ilya <sckremyulock@gmail.com>
2023-04-17 17:50:26 +0500

    task5

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const modalOverlay = document.getElementById("modal-overlay");
 const modal = document.getElementById("modal");
 const addBeverageButton = document.querySelector('.add-button');
 const closeBtn = document.getElementById("close-btn");
+let differentValues = new Set([2, 3, 4]);
+let differentValues2 = new Set([12, 13, 14]);
+let differentValues3 = new Set([1]);
 
 
 doneBtn.addEventListener("click", (e) => {
-    console.log(modal, modalOverlay);
     modalOverlay.classList.remove("hidden");
     modal.classList.remove("hidden");
+    let p = document.getElementById('count_beverage');
+    console.log(p);
+    if (differentValues.has(numBeverages % 10) && !differentValues2.has(numBeverages % 100)) {
+        p.textContent = `Вы заказали ${numBeverages} напитка`;
+    }
+    else if (differentValues3.has(numBeverages % 10) && !differentValues2.has(numBeverages % 100)) {
+        p.textContent = `Вы заказали ${numBeverages} напиток`;
+    }
+    else {
+        p.textContent = `Вы заказали ${numBeverages} напитков`;
+    }
 });
 addBeverageButton.addEventListener('click', addBeverage);
 
