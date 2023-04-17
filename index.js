3469e9f28889aab842417a5de2fa7bbb61ec5324
annet <Anna.Kirianova@urfu.me>
2023-04-17 14:48:33 +0500

    All

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ addButton.addEventListener('click', e => {
     clone.querySelector('.beverage-count').textContent = `Напиток №${countOnClickAddButton}`;
     beverage.after(clone);
 })
+
+nums234 = ['2', '3', '4']
+num1 = ['1']
+function handle(countOnClickAddButton) {
+    let strNum = countOnClickAddButton.toString().slice(-2);
+    if (countOnClickAddButton < 10) strNum = '0' + strNum;
+    let correctEnd = strNum[0] !== '1' && nums234.includes(strNum[1]) ? 'ка' :
+        strNum[0] !== '1' && num1.includes(strNum[1]) ? 'ок' : 'ков';
+    console.log(`Вы заказали ${countOnClickAddButton} напит${correctEnd}`);
+}
+
+function remove(form){
+    if (document.querySelectorAll('.beverage').length > 1) {
+        form.parentElement.remove();
+    }
+}
+
+let modal = document.getElementsByClassName('modal')[0];
+let btn = document.getElementsByClassName('submit-button')[0];
+let span = document.getElementsByClassName("close")[0];
+
+btn.addEventListener("click" , (event) => {
+    event.preventDefault();
+    modal.style.display = "block";
+});
+
+span.addEventListener("click", () => {
+    modal.style.display = "none";
+});
