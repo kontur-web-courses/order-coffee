9cd58097064f1f3fd7a5b0402a7bd6751cf2e39e
Pavel Reshetnikov <pasha.keyzet@yandex.ru>
2023-04-17 18:10:24 +0500

    Запись в модальном окне

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const form = document.querySelector('form');
 
 let previousBeverage = form.firstElementChild;
 let beverageCount = 1;
+updateModalText();
 
 addButton.addEventListener('click', () => {
   const newBeverage = previousBeverage.cloneNode(true);
@@ -11,6 +12,7 @@ addButton.addEventListener('click', () => {
   newBeverage.querySelector('.beverage-count').textContent = `Напиток №${beverageCount}`;
   previousBeverage.after(newBeverage);
   previousBeverage = newBeverage;
+    updateModalText();
 });
 
 
@@ -24,6 +26,28 @@ submitButton.addEventListener('click', event => {
 
 const closeButton = document.querySelector('.modal-close');
 
-closeButton.addEventListener('click', function() {
-  modal.style.display = 'none';
-});
+closeButton.addEventListener('click', () => {
+    updateModalText();
+    modal.style.display = 'none';
+  });
+
+  function pluralize(number, wordForms) {
+    const cases = [2, 0, 1, 1, 1, 2];
+    let wordFormIndex;
+  
+    if (number % 100 > 4 && number % 100 < 20) {
+      wordFormIndex = 2;
+    } else {
+      if (number % 10 < 5) {
+        wordFormIndex = cases[number % 10];
+      } else {
+        wordFormIndex = 2;
+      }
+    }
+  
+    return wordForms[wordFormIndex];
+  }
+
+function updateModalText() {
+    document.querySelector('.modal-text').textContent = `Вы заказали ${beverageCount} ${pluralize(beverageCount, ['напиток', 'напитка', 'напитков'])}`;
+}
