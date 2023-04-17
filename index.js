a43a3fc3e1a005d9daca94a42c055d22c8efeab4
TashK101 <Natalia.Karimova@urfu.me>
2023-04-17 14:36:33 +0500

    button

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 let count = 1;
 const button = document.querySelector('.add-button');
 const fieldset = document.querySelector('.beverage');
+let deleteButtons = document.querySelectorAll('.delete-button');
+deleteButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const beverageFieldset = button.parentElement;
+        if (count > 1) {
+            beverageFieldset.remove();
+            count--;
+            console.log(1)
+        }
+    });
+});
 button.addEventListener('click', function (){
     count++;
     let newFieldset = fieldset.cloneNode(true)
     newFieldset.querySelector('.beverage-count').textContent = `Напиток №${count}`;
+
     fieldset.after(newFieldset);
 });
+
+
+
+
