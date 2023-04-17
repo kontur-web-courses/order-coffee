a33c421345d4bab687367b6917810eb32094a122
Stup <meadow174@gmail.com>
2023-04-17 15:48:28 +0500

    create modal

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let addForm = document.querySelector("button.add-button");
 let countForms = 1;
 addForm.addEventListener("click", () => {
@@ -11,14 +10,34 @@ addForm.addEventListener("click", () => {
     let head4 = newFormCoffee.querySelector("h4.beverage-count");
     head4.innerHTML = `Напиток №${countForms}`;
 
-    for (let i = 0; i < options.length; i++){
+    for (let i = 0; i < options.length; i++) {
         options[i].name = `milk${countForms}`;
     }
 });
 
-function remove(form){
-    if (countForms > 1){
+function remove(form) {
+    if (countForms > 1) {
         form.parentElement.remove();
         countForms -= 1;
     }
 }
+
+let modalCloseButton = document.querySelector("span.remove-button");
+let modalWindow = document.querySelector("div.modal");
+modalCloseButton.addEventListener('click', () => {
+    modalWindow.style.display = 'none';
+});
+
+let submit = document.querySelector("button.submit-button");
+submit.addEventListener('click', event => {
+    event.preventDefault();
+    modalWindow.style.display = 'block';
+    let drinkWord = 'напитков';
+    if (countForms % 100 < 11 || countForms % 100 > 19)
+        drinkWord = 'напитка'
+
+    else if (countForms % 10 === 1 && countForms % 100 !== 11)
+        drinkWord = 'напиток';
+
+    document.querySelector('p.drinkCount').innerText = `Вы заказали ${countForms} ${drinkWord}`;
+})
