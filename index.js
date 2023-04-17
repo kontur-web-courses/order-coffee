76746c4a0ce7f461a67381159b2e4054e4fd7fc3
bolthen <92172378+bolthen@users.noreply.github.com>
2023-04-17 14:45:35 +0500

    add drinks info str

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const createDrinkButton = document.getElementById('submit-button');
 const modalWindow = document.getElementById('modal-window');
 const modalCloseButton = document.getElementById('modal-close');
 const overlay = document.getElementById('fixed-overlay');
+const drinksCountInfoSpan = document.getElementById('drinks-count');
+
+
+function getDrinksInfoStr() {
+    let result = `${drinksCount} напит`;
+
+    if (drinksCount % 10 === 1 && drinksCount !== 11)
+        result += 'ок';
+    else if (2 <= drinksCount % 10 && drinksCount % 10 <= 4 && Math.floor(drinksCount / 10) % 10 !== 1)
+        result += 'ка';
+    else
+        result += 'ков';
+
+    return result;
+}
 
 function createModalWindow(event) {
-    ++drinksCount;
+    drinksCountInfoSpan.textContent = getDrinksInfoStr();
     modalWindow.style.display = 'block';
     overlay.style.display = 'block';
 }
