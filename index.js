ee4866ac6272d8bcc69e964c99a5c58ccac9e37f
natosafix <92174064+natosafix@users.noreply.github.com>
2023-04-17 15:14:35 +0500

    1, 3-6

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-
 let orderCount = 1;
+
+function closeLightbox() {
+    document.getElementById("blur_panel").style.visibility = "hidden";
+    document.getElementById("lightbox").style.visibility = "hidden";
+}
+
+function openLightbox() {
+    document.getElementById("blur_panel").style.visibility = "visible";
+    document.getElementById("lightbox").style.visibility = "visible";
+    updateInfoAboutOrder();
+}
+
+function updateInfoAboutOrder() {
+    let lightbox = document.getElementById("lightbox");
+    let p = lightbox.querySelector('p');
+    let text = `\nВы заказали ${orderCount} `;
+    if ([2, 3, 4].includes(orderCount % 10) && Math.floor(orderCount / 10) % 10 !== 1)
+        text += 'напитка.';
+    else if(orderCount % 10 == 1 && Math.floor(orderCount / 10) % 10 !== 1)
+        text += 'напиток.';
+    else
+        text += 'напитков.';
+    p.innerText += text;
+}
+
 let exampleOrder = document.querySelector('.beverage').cloneNode(true);
 document.querySelector('.add-button')
     .addEventListener('click', () => createNewForm());
