e7dafb5be41725cb7b2a84591129b2a51f6a1341
Lada Kuzmina <lada.kuzmina2003@gmail.com>
2023-04-17 20:26:26 +0500

    add cross

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 let addButton = document.querySelector('.add-button');
 let counter = 1;
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', function () {
     let beverage = document.getElementById(`fieldset${counter}`);//querySelector(`.beverage${counter-1}`);
     let newNode = beverage.cloneNode(true);
     newNode.id = `fieldset${counter + 1}`;
@@ -10,26 +9,23 @@ addButton.addEventListener('click', function() {
     console.log(counter);
     counter++;
 });
-
-document.querySelector('.submit-button').addEventListener('click',function(e){
+document.querySelector('.submit-button').addEventListener('click', function (e) {
     let modal = document.getElementById('modal');
     let textNode = document.createTextNode(`Вы заказали ${counter} напитков`);
-    if (counter % 10 === 1 && counter != 11){
+    if (counter % 10 === 1 && counter !== 11) {
         textNode = document.createTextNode(`Вы заказали ${counter} напиток`);
-    }
-    else if ((counter % 10 === 2 || counter % 10 === 3 || counter % 10 === 4) && counter != 12 && counter != 13 && counter != 14 ){
+    } else if ((counter % 10 === 2 || counter % 10 === 3 || counter % 10 === 4) && counter !== 12 && counter !== 13 && counter !== 14) {
         textNode = document.createTextNode(`Вы заказали ${counter} напитка`);
-    }
-    else{
+    } else {
         textNode = document.createTextNode(`Вы заказали ${counter} напитков`);
     }
-    
+
     document.querySelector('.modal__content').appendChild(textNode);
     e.preventDefault();
     modal.classList.add('modal_active');
 });
 
-document.querySelector('.modal__close-button').addEventListener('click', function(e){
+document.querySelector('.modal__close-button').addEventListener('click', function (e) {
     let modal = document.getElementById('modal');
     modal.classList.remove('modal_active');
     location.reload();
