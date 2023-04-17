4d6722f783cddc90887f4ad5db4caedff3c2edf8
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2023-04-17 17:52:05 +0500

    submit button

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let formsCount = 1;
 let currFormNum = 1
 
@@ -15,20 +14,31 @@ function addToCrossCloseEvent(cross){
     });
 }
 
-
-function AddButtonListener() {
+function AddNewForm() {
     formsCount++;
     let form = document.getElementsByTagName('form')[0].cloneNode(true);
     let header = form.getElementsByClassName('beverage-count')[0];
     let button = form.getElementsByClassName('add-button')[0];
     let cross = form.getElementsByClassName('form-close')[0];
     addToCrossCloseEvent(cross);
-    button.addEventListener('click', AddButtonListener);
+    button.addEventListener('click', AddNewForm);
     header.textContent = `Напиток №${++currFormNum}`;
     document.body.appendChild(form);
 }
 
+function Submit() {
+    const modalBackground = document.getElementsByClassName("modalBackground")[0].cloneNode(true);
+    const modalActive = document.getElementsByClassName("modalActive")[0].cloneNode(true);
+    const modalWindow = document.getElementsByClassName("modalWindow")[0].cloneNode(true);
+    document.body.appendChild(modalBackground);
+    document.body.appendChild(modalActive);
+    document.body.appendChild(modalWindow);
+}
 
 addToCrossCloseEvent(document.getElementsByClassName('form-close')[0]);
+
 let addButton = document.getElementsByClassName('add-button')[0];
-addButton.addEventListener('click', AddButtonListener);
+addButton.addEventListener('click', AddNewForm);
+
+let submitButton = document.getElementsByClassName('submit-button')[0];
+submitButton.addEventListener('click', Submit);
