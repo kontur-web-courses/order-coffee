1bd0c10adbba88ed7399818ea2ceeafa1de79fa2
ArinaBalakina <Balakinaarina@yandex.ru>
2023-04-17 15:06:16 +0500

    button_close

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+
+function closeClone(button){
+    const parent = button.parentElement;
+    if (countOrders>1){
+        parent.remove()
+        countOrders--;
+    }
+}
+
 let countOrders = 1;
+
 document.querySelector(".overlay").style.display = "none";
 document.querySelector(".add-button").addEventListener("click", function () {
     let beverage = document.querySelector(".beverage");
     let clone = beverage.cloneNode(true);
     countOrders++;
-    clone.querySelector(".beverage-count").textContent = `Напито №${countOrders}`;
+    clone.querySelector(".beverage-count").textContent = `Напиток №${countOrders}`;
     beverage.after(clone);
 })
+
 
 document.querySelector(".submit-button").addEventListener("click", function () {
     document.querySelector(".overlay").style.display = "flex";
