af13277a7f661746bdfe88f7aa094c807b4e1a37
AndreyKuklinov <Andreydolphin@mail.ru>
2023-04-17 17:59:47 +0500

    task two

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let numBeverages = 1;
+let totalBeverages = 1;
+let curNumBeverages = 1;
 
 const doneBtn = document.getElementById("submit-button");
 const modalOverlay = document.getElementById("modal-overlay");
 const modal = document.getElementById("modal");
 const addBeverageButton = document.querySelector('.add-button');
+const beverage = document.querySelector('.beverage');
 
 doneBtn.addEventListener("click", (e) => {
     console.log(modal, modalOverlay);
@@ -11,13 +13,32 @@ doneBtn.addEventListener("click", (e) => {
     modal.classList.remove("hidden");
 });
 addBeverageButton.addEventListener('click', addBeverage);
+initBeverage(beverage);
 
+function initBeverage(beverage) {
+  const removeBeverageButton = beverage.querySelector('.remove-beverage');
+  removeBeverageButton.addEventListener('click', (e) => {
+    removeBeverage(e);
+  });
+}
 
 function addBeverage() {
     const beverage = document.getElementsByClassName('beverage')[0];
     const clonedBeverage = beverage.cloneNode(true);
     const clonedCountObj = clonedBeverage.querySelectorAll(".beverage-count")[0];
-    clonedCountObj.textContent = `Напиток №${++numBeverages}`;
+    clonedCountObj.textContent = `Напиток №${++totalBeverages}`;
     const form = document.getElementsByTagName('form')[0];
     form.insertBefore(clonedBeverage, form.children[form.children.length-1]);
+    initBeverage(clonedBeverage);
+    curNumBeverages++;
+}
+
+function removeBeverage(e) {
+    if(curNumBeverages <= 1) {
+        return;
+    }
+    
+    const bev = e.target.parentNode;
+    bev.parentNode.removeChild(bev);
+    curNumBeverages--;
 }
