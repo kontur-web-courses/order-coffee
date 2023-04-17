d32b2b4afa6f799702c344e1adba928d76dd64d1
Maxim <maxim.chevychelov@gmail.com>
2023-04-17 14:18:27 +0500

    3

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 let count = 1;
 const button = document.querySelector('.add-button');
-const fieldset = document.querySelector('.beverage');
+const readyButton = document.querySelector('.submit-button');
+let fieldset = document.querySelector('.beverage');
+const modal = document.querySelector('.hystmodal')
+modal.querySelector('.hystmodal__close').addEventListener('click',
+    function () {
+    modal.style.display = 'none';
+    })
+readyButton.addEventListener('click', function (){
+    modal.style.display = 'flex';
+});
+
 button.addEventListener('click', function (){
     count++;
     let newFieldset = fieldset.cloneNode(true)
     newFieldset.querySelector('.beverage-count').textContent = `Напиток №${count}`;
     fieldset.after(newFieldset);
+    fieldset = newFieldset;
 });
