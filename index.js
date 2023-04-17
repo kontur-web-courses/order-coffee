8d98b286144a49d4b0fe1654e309387d0820ee44
Maxim <maxim.chevychelov@gmail.com>
2023-04-17 15:37:36 +0500

    7

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const button = document.querySelector('.add-button');
 const readyButton = document.querySelector('.submit-button');
 let fieldset = document.querySelector('.beverage');
 const modal = document.querySelector('.hystmodal')
+
 modal.querySelector('.hystmodal__close').addEventListener('click',
     function () {
     modal.style.display = 'none';
@@ -27,6 +28,21 @@ readyButton.addEventListener('click', function (){
     modal.querySelector('.accept').textContent = `Вы заказали ${count} ${getRightDrinks(count)}!`;
     modal.style.display = 'flex';
 });
+
+function HandleTextArea(area){
+    const words = ["срочно", "быстрее", "побыстрее", "скорее", "поскорее", "очень нужно"]
+    const text = area.parentElement.querySelector('.changedAreaValue');
+    let areaValue = area.value;
+    for (let word of words){
+        if (!areaValue.includes(word)) continue;
+        let fIndex = areaValue.search(word);
+        areaValue = areaValue.substring(0, fIndex) + '<b>'
+            + areaValue.substring(fIndex, fIndex + word.length)
+            + '</b>' + areaValue.substring(fIndex + word.length);
+    }
+    text.innerHTML = areaValue;
+}
+
 function Delete(button)
 {
     const beverageFieldset = button.parentElement;
@@ -40,6 +56,8 @@ function Delete(button)
 button.addEventListener('click', function (){
     count++;
     let newFieldset = fieldset.cloneNode(true)
+    newFieldset.querySelector('.changedAreaValue').textContent = '';
+    newFieldset.querySelector('.textArea').value = '';
     newFieldset.querySelector('.beverage-count').textContent = `Напиток №${count}`;
     fieldset.after(newFieldset);
     fieldset = newFieldset;
