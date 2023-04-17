644d477f7ddf0da111928552e386d79c91c7b079
andrei-uni <92112053+andrei-uni@users.noreply.github.com>
2023-04-17 15:17:56 +0500

    1-7

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+for (let button of document.querySelectorAll(".close-button")) {
+    button.addEventListener("click", function (event) {
+       const count = document.querySelectorAll("form").length;
+       if (count > 1) {
+           button.closest("form").remove();
+       }
+    });
+}
+
+for (let area of document.querySelectorAll("textarea")) {
+    area.addEventListener("input", function (event) {
+        const boldWords = ["срочно", "быстрее", "побыстрее", "скорее", "поскорее"];
+        const boldWords2 = ["очень нужно"];
+
+        const s = event.target.value.split(' ');
+
+        function toBold(value) {
+            return `<b>${value}</b> `;
+        }
+
+        let content = "";
+        const len = s.length;
+        let needSkip = false;
+
+        for (let i = 0; i < len; i++) {
+            if (needSkip) {
+                needSkip = false;
+                continue;
+            }
+            if (i !== len - 1) {
+                const phrase = s[i] + " " + s[i+1];
+                if (boldWords2.includes(phrase)) {
+                    content += toBold(phrase);
+                    needSkip = true;
+                    continue;
+                }
+            }
+
+            const word = s[i];
+
+            if (!boldWords.includes(word)) {
+                content += word + " ";
+                continue;
+            }
+
+            content += toBold(word);
+        }
+
+        area.closest(".extra-input").querySelector(".bold-output").remove();
+
+        const span = document.createElement("span");
+        span.className = "bold-output";
+        span.innerHTML = content;
+
+        document.querySelector(".extra-input").append(span);
+    });
+}
+
 let orderCount = 1;
 
 function closeLightbox() {
@@ -39,6 +97,7 @@ function createNewForm(){
             querySelectorElementElement.name = `milk${orderCount}`;
         }
     }
+    // newOrder.getElementsByName(`milk${orderCount}`).forEach(x => x.name = `milk${++orderCount}`);
 
     newOrder.querySelector('.beverage-count').innerText = `Напиток №${orderCount}`;
 
